7b9747890a47cd5d459821a0c8d1eda0ee9fab57
Clement Barbaza <clement.barbaza@gmail.com>
2020-08-30 19:51:14 +0200

    add flash message test

--- tests/Session/FlashTest.php
+++ tests/Session/FlashTest.php
@@ -9,20 +9,42 @@ use Icarus\Support\Facades\Session;
 final class FlashTest extends TestCase
 {
     /**
+     * Test value
+     *
+     * @var array
+     */
+    protected $value = ['name' => 'Clément', 'email' => 'clement@test.com'];
+
+    /**
      * Test create a flash message
      *
      * @return void
      */
     public function testCreateFlashMessage()
     {
-        $value = ['name' => 'Clément', 'email' => 'clement@test.com'];
-        Flash::create('input', $value);
+        Flash::create('input', $this->value);
         $this->assertIsArray(Flash::get('input'));
         $this->assertArrayHasKey('name', Flash::get('input'));
         $this->assertEquals('Clément', Flash::get('input')['name']);
         $this->assertArrayHasKey('email', Flash::get('input'));
         $this->assertEquals('clement@test.com', Flash::get('input')['email']);
         $this->assertIsArray(Session::get('icarus-plugin-flash')['input']);
-        $this->assertEquals($value, Session::get('icarus-plugin-flash')['input']);
+        $this->assertEquals($this->value, Session::get('icarus-plugin-flash')['input']);
+    }
+
+    /**
+     * Test get flash message
+     *
+     * @return void
+     */
+    public function testGetFlashMessage()
+    {
+        $this->assertIsArray(Flash::get('input'));
+        $this->assertArrayHasKey('name', Flash::get('input'));
+        $this->assertEquals('Clément', Flash::get('input')['name']);
+        $this->assertArrayHasKey('email', Flash::get('input'));
+        $this->assertEquals('clement@test.com', Flash::get('input')['email']);
+        $this->assertIsArray(Session::get('icarus-plugin-flash')['input']);
+        $this->assertEquals($this->value, Session::get('icarus-plugin-flash')['input']);
     }
 }
